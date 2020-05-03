404d4bd8c3827a82c5913af0fefc245f440e82be
alexjperkins <alexjperkins@hotmail.co.uk>
2020-05-03 16:30:04 +0100

    chore: project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react';
+import { ApolloProvider } from '@apollo/react-hooks'
+import { ThemeProvider } from '@material-ui/core'
+import { BrowserRouter } from 'react-router-dom'
+
+import { gqlClient } from './clients'
+import { theme } from './theme'
+
+import './App.css'
+import { LandingPage } from './pages/LandingPage'
+
+const Loader = () => (
+  <div className="App">
+    <div>loading...</div>
+  </div>
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<Loader />}>
+      <ApolloProvider client={gqlClient}>
+          <ThemeProvider theme={theme}>
+            <BrowserRouter>
+              <LandingPage />
+            </BrowserRouter>
+        </ThemeProvider>
+      </ApolloProvider>
+    </Suspense>
   );
 }
 
